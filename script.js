671446779ce3ea1fc5220479dc84681f27588727
lacey aguigam <laceyaguigam@yahoo.com>
2023-12-04 19:15:52 -0800

    created comment notes for different parts of the calculator

---
+++ script.js
@@ -0,0 +1,18 @@
+
+
+// buttons for numbers 0-9
+
+
+//button for equal 
+
+
+//button for +, -, /, *
+
+//show last equation 
+
+//be able to add multiple numbers together including repeat 
+
+//button to clear 
+
+
+// show numbers after pressed 
