1d62e9ae28c38e1cd692f25175c71bf13411b451
lacey aguigam <laceyaguigam@yahoo.com>
2023-12-04 22:01:07 -0800

    fixed the button variable, element needed an s at the end for elements

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 
 const display = document.getElementById("display");  // for display at the top
 const lastExpression = document.getElementById("last-expression");  // for display at bottom to show the last expression
-const button = document.getElementByClassName("calculator-button"); // for each button on the calculator 
+const button = document.getElementsByClassName("calculator-button"); // for each button on the calculator 
 
 
 
@@ -20,7 +20,7 @@ function showDisplay() {
 //will also need a function to update the display 
 
 function displayUpdate() {
-    
+
 }
 
 
