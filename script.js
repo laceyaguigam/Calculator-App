10ce62343f78938986df8ff447cb0247c53c666e
lacey aguigam <laceyaguigam@yahoo.com>
2023-12-05 20:54:43 -0800

    added the final requirements

--- script.js
+++ script.js
@@ -17,6 +17,8 @@ let result = " "; // this will be the answer to the expression
 
 function showDisplay() {
 
+        //this will need .innerText
+display.innerText = display?
 }
 
 //will also need a function to update the display 
@@ -94,3 +96,45 @@ switch(value1, value2) {
     default:
         console.log("ERROR");
 }
+
+
+
+
+
+
+
+
+
+
+// The user should be able to enter a simple expression:
+
+// The user should be able to choose the first number, which should appear in the expression box.
+
+// The user should be able to choose the desired operation (addition, subtraction, multiplication, division) and the appropriate operator should appear in the expression box.
+
+// The user should be able to choose the second number, which should appear in the expression box.
+
+// The user should not be able to choose an operation before adding the first number.
+
+// The user should be able to see the calculation result after clicking an equals button.
+
+// The user should be able to clear the expression after clicking a clear button.
+
+// The user should not be able to pad with zeroes.
+
+// Essentially, ignore more than one zero without any other number being entered.
+// Valid: 0
+// Invalid: 00, 001, 00009, etc.
+
+// All calculator buttons except for “clear” should be disabled after the expression has been evaluated (the “equals” button has been pressed).
+
+// You should re-enable the buttons after the user clicks the “clear” button again.
+
+// The calculator display should be updated in real-time, showing the numbers and the operator as the user clicks the buttons.
+
+// You should keep track of the last expression and the result of the last expression, which should persist after a page refresh.
+
+// You may choose to use local or session storage for this.
+
+// This should update as soon as the “equals” button is pressed.
+
